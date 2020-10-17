d828b0760fd4776141deaeeb85f34d0a9e959986
sghodsi <sghodsi@ncsu.edu>
2020-10-17 14:40:28 -0400

    remove is_mem_write

--- src/clients/drcctlib_drdeadspy/drcctlib_drdeadspy.cpp
+++ src/clients/drcctlib_drdeadspy/drcctlib_drdeadspy.cpp
@@ -265,8 +265,8 @@ InstrumentInsCallback(void *drcontext, instr_instrument_msg_t *instrument_msg)
 
     if (is_mem) {
         DRCCTLIB_PRINTF("******InstrumentInsCallback6");
-        bool is_mem_write = instr_writes_memory(instr);
-        DRCCTLIB_PRINTF("******is_mem_write");
+        // bool is_mem_write = instr_writes_memory(instr);
+        // DRCCTLIB_PRINTF("******is_mem_write");
         // dr_insert_clean_call(drcontext, bb, instr, (void *)InsertMemCleancall, false, 3,
         //                      OPND_CREATE_CCT_INT(slot), OPND_CREATE_CCT_INT(num),
         //                      is_mem_write);
