86047f5e07cdd2e9a3021e060c35777ad4bfaf6b
sghodsi <sghodsi@ncsu.edu>
2020-10-17 16:06:03 -0400

    comment everything again

--- src/clients/drcctlib_drdeadspy/drcctlib_drdeadspy.cpp
+++ src/clients/drcctlib_drdeadspy/drcctlib_drdeadspy.cpp
@@ -213,18 +213,18 @@ InstrumentInsCallback(void *drcontext, instr_instrument_msg_t *instrument_msg)
             is_mem = true;
         }
 
-        if (opnd_is_reg(op)) {
-            // DRCCTLIB_PRINTF("******InstrumentInsCallback2");
-            int num_temp = opnd_num_regs_used(op);
-            for (int j = 0; j < num_temp; j++) {
-                // DRCCTLIB_PRINTF("******num_temp=%d", num_temp);
-                reg_id_t reg = opnd_get_reg_used(op, j);
-                // DRCCTLIB_PRINTF("******reg=%d", reg);
-                dr_insert_clean_call(drcontext, bb, instr, (void *)InsertRegCleancall,
-                                     false, 3, OPND_CREATE_CCT_INT(slot), reg, false);
-                // DRCCTLIB_PRINTF("******dr_insert_clean_call1");
-            }
-        }
+        // if (opnd_is_reg(op)) {
+        //     // DRCCTLIB_PRINTF("******InstrumentInsCallback2");
+        //     int num_temp = opnd_num_regs_used(op);
+        //     for (int j = 0; j < num_temp; j++) {
+        //         // DRCCTLIB_PRINTF("******num_temp=%d", num_temp);
+        //         reg_id_t reg = opnd_get_reg_used(op, j);
+        //         // DRCCTLIB_PRINTF("******reg=%d", reg);
+        //         dr_insert_clean_call(drcontext, bb, instr, (void *)InsertRegCleancall,
+        //                              false, 3, OPND_CREATE_CCT_INT(slot), reg, false);
+        //         // DRCCTLIB_PRINTF("******dr_insert_clean_call1");
+        //     }
+        // }
     }
     for (int i = 0; i < instr_num_dsts(instr); i++) {
         // DRCCTLIB_PRINTF("******InstrumentInsCallback3");
@@ -237,41 +237,41 @@ InstrumentInsCallback(void *drcontext, instr_instrument_msg_t *instrument_msg)
             InstrumentMem(drcontext, bb, instr, op);
             is_mem = true;
 
-            int num_temp = opnd_num_regs_used(op);
-            for (int j = 0; j < num_temp; j++) {
-                // DRCCTLIB_PRINTF("******InstrumentInsCallback4");
-                // DRCCTLIB_PRINTF("******num_temp=%d", num_temp);
-                reg_id_t reg = opnd_get_reg_used(op, j);
-                // DRCCTLIB_PRINTF("******reg=%d", reg);
-                dr_insert_clean_call(drcontext, bb, instr, (void *)InsertRegCleancall,
-                                     false, 3, OPND_CREATE_CCT_INT(slot), reg, false);
-                // DRCCTLIB_PRINTF("******dr_insert_clean_call2");
-            }
+            // int num_temp = opnd_num_regs_used(op);
+            // for (int j = 0; j < num_temp; j++) {
+            //     // DRCCTLIB_PRINTF("******InstrumentInsCallback4");
+            //     // DRCCTLIB_PRINTF("******num_temp=%d", num_temp);
+            //     reg_id_t reg = opnd_get_reg_used(op, j);
+            //     // DRCCTLIB_PRINTF("******reg=%d", reg);
+            //     dr_insert_clean_call(drcontext, bb, instr, (void *)InsertRegCleancall,
+            //                          false, 3, OPND_CREATE_CCT_INT(slot), reg, false);
+            //     // DRCCTLIB_PRINTF("******dr_insert_clean_call2");
+            // }
         }
 
-        if (opnd_is_reg(op)) {
-            // DRCCTLIB_PRINTF("******InstrumentInsCallback5");
-            int num_temp = opnd_num_regs_used(op);
-            for (int j = 0; j < num_temp; j++) {
-                // DRCCTLIB_PRINTF("******num_temp=%d", num_temp);
-                reg_id_t reg = opnd_get_reg_used(op, j);
-                // DRCCTLIB_PRINTF("******reg=%d", reg);
-                dr_insert_clean_call(drcontext, bb, instr, (void *)InsertRegCleancall,
-                                     false, 3, OPND_CREATE_CCT_INT(slot), reg, true);
-                // DRCCTLIB_PRINTF("******dr_insert_clean_call3");
-            }
-        }
+        // if (opnd_is_reg(op)) {
+        //     // DRCCTLIB_PRINTF("******InstrumentInsCallback5");
+        //     int num_temp = opnd_num_regs_used(op);
+        //     for (int j = 0; j < num_temp; j++) {
+        //         // DRCCTLIB_PRINTF("******num_temp=%d", num_temp);
+        //         reg_id_t reg = opnd_get_reg_used(op, j);
+        //         // DRCCTLIB_PRINTF("******reg=%d", reg);
+        //         dr_insert_clean_call(drcontext, bb, instr, (void *)InsertRegCleancall,
+        //                              false, 3, OPND_CREATE_CCT_INT(slot), reg, true);
+        //         // DRCCTLIB_PRINTF("******dr_insert_clean_call3");
+        //     }
+        // }
     }
 
-    if (is_mem) {
-        // DRCCTLIB_PRINTF("******InstrumentInsCallback6");
-        bool is_mem_write = instr_writes_memory(instr);
-        // DRCCTLIB_PRINTF("******is_mem_write");
-        dr_insert_clean_call(drcontext, bb, instr, (void *)InsertMemCleancall, false, 3,
-                             OPND_CREATE_CCT_INT(slot), OPND_CREATE_CCT_INT(num),
-                             is_mem_write);
-        // DRCCTLIB_PRINTF("******dr_insert_clean_call4");
-    }
+    // if (is_mem) {
+    //     // DRCCTLIB_PRINTF("******InstrumentInsCallback6");
+    //     bool is_mem_write = instr_writes_memory(instr);
+    //     // DRCCTLIB_PRINTF("******is_mem_write");
+    //     dr_insert_clean_call(drcontext, bb, instr, (void *)InsertMemCleancall, false, 3,
+    //                          OPND_CREATE_CCT_INT(slot), OPND_CREATE_CCT_INT(num),
+    //                          is_mem_write);
+    //     // DRCCTLIB_PRINTF("******dr_insert_clean_call4");
+    // }
 
     // DRCCTLIB_PRINTF("******InstrumentInsCallback7");
 }
