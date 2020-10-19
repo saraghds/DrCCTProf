1cb21b082f779fae7ad45b1be50c7d5081f41488
sghodsi <sghodsi@ncsu.edu>
2020-10-19 19:24:20 -0400

    change test-sample

--- test_apps/src/test-sample.cpp
+++ test_apps/src/test-sample.cpp
@@ -1,146 +1,221 @@
-#include <iostream>
-#include <random>
-#include <string>
+#include <bits/stdc++.h>
+#include <stdlib.h>
 
-struct player
+using namespace std;
+
+struct node
 {
-    std::string name;
-    int total_money{ 0 };
+	int vertex;
+	node* next;
 };
 
-struct wager_result
+struct Graph
 {
-    bool did_win = false;
-    int money_change_amount{ 0 };
-    int wager{ 0 };
-    int guess{ 0 };
+	int numVertices;
+	node** adjLists;
 };
-std::ostream& operator<<(std::ostream& output, const wager_result &wager)
+
+typedef struct 
 {
-    output << "You bet " << std::to_string(wager.wager) << " and guessed "
-        << std::to_string(wager.guess) << std::endl;
-    output << "You ";
-    if(wager.did_win)
-    {
-        output << "won ";
-    }
-    else
-    {
-        output << "lost ";
-    }
-    output << std::to_string(std::abs(wager.money_change_amount)) << std::endl;
-    return output;
+	int from;
+	int to;
+	int weight;	
+}Edge;
+
+bool compare_edge(Edge edge1,Edge edge2)
+{
+	if(edge1.from < edge2.from)
+		return 0;
+	if(edge2.from < edge1.from)
+		return 1;
+	if(edge1.weight < edge2.weight)
+		return 0;
+	if(edge2.weight < edge1.weight)
+		return 1;
+	if(edge1.to < edge2.to)
+		return 0;
+
+	return 1;
 }
 
-class number_generator
+void merge(Edge *edge,int a,int b)
 {
-public:
-    number_generator(const int &min, const int &max)
-        : random_engine_(random_device_()), uniform_int_distribution_(min, max)
-    {
+	int i,j,count=a;
+	i = a;
+	j = (a+b)/2 + 1;
 
-    }
-    int generate_random_number()
-    {
-        return uniform_int_distribution_(random_engine_);
-    }
-private:
-    std::random_device random_device_;
-    std::default_random_engine random_engine_;
-    std::uniform_int_distribution<int> uniform_int_distribution_;
-};
+	Edge temp[b-a+1];
 
-class game
+	for (int k = a; k <= b; ++k)
+	{
+		temp[k-a] = edge[k]; 
+	}
+
+	while(i!= (a+b/2)+1 && j!=(b+1))
+	{
+		if(compare_edge(temp[i-a],temp[j-a]))
+		{
+			edge[count++] = temp[j-a];
+			j++;
+		}
+		else
+		{
+			edge[count++] = temp[i-a];
+			i++;
+		}
+	}
+
+	if(i!=(a+b)/2 +1)
+	{
+		while(i!=(a+b)/2 + 1)
+		{
+			edge[count++] = temp[i-a];
+			i++;
+		}
+
+	}
+	else if(j!=(b+1))
+	{
+		while(j!=(b+1))
+		{
+			edge[count++] = temp[j-a];
+			j++;
+		}
+	}
+}
+
+// a and b are indices
+
+void merge_sort(Edge *edge,int a,int b)
 {
-    player player_;
-    number_generator number_generator_;
-public:
-    game(const player& player)
-        :number_generator_(1, 10)
-    {
-        player_ = player;
-    }
+	if(a==b)
+		return;
 
-    void start_game()
-    {
-        std::cout << "Enter your name: ";
-        std::getline(std::cin, player_.name);
-        std::cout << "Enter start money amount: ";
-        std::cin >> player_.total_money;
-    }
-
-    bool keep_playing()
-    {
-        std::cout << "Play again? (Y/N): ";
-        char response;
-        std::cin >> response;
-        return response == 'y' || response == 'Y';
-    }
-
-    int get_bet_amount()
-    {
-        std::cout << "Enter bet amount (1 - " +
-            std::to_string(player_.total_money) + "): ";
-        int wager;
-        std::cin >> wager;
-        return wager;
-    }
-    
-    int get_guess()
-    {
-        std::cin.get();
-        std::cout << "Enter guess (1-10): ";
-        int guess;
-        std::cin >> guess;
-        return guess;
-    }
-    wager_result play_round()
-    {
-        const auto wager = get_bet_amount();
-        const auto guess = get_guess();
-
-        wager_result result;
-        result.wager = wager;
-        result.guess = guess;
-        const auto number = number_generator_.generate_random_number();
-        if(number == guess)
-        {
-            result.did_win = true;
-            result.money_change_amount = wager * 10;
-        }
-        else
-        {
-            result.did_win = false;
-            result.money_change_amount = -1 * wager;
-        }
-
-        player_.total_money += result.money_change_amount;
-        return result;
-    }
-
-    bool game_over()
-    {
-        return player_.total_money <= 0;
-    }
-};
+	merge_sort(edge,a, (a+b)/2);
+	merge_sort(edge,(a+b)/2 +1,b);
+	merge(edge,a,b);
+}
 
 int main()
 {
-    std::cout << "----- Casino! -----" << std::endl;
-    const player m_player;
-    game game(m_player);
-    game.start_game();
-    do
-    {
-        const auto result = game.play_round();
-        std::cout << result << std::endl;
-        if (game.game_over()) {
-            std::cout << "You lost, game over" << std::endl;
-            break;
-        }
+	ifstream input;
+	ofstream output;
 
-    } while (!game.game_over() && game.keep_playing());
+	input.open("input.txt");
+	output.open("output.txt");
 
-    std::cout << "Game over! Thanks for playing!" << std::endl;
-    return 0;
+	int V,E;
+	input>>V;
+	input>>E;
+
+	// cout<<"V: "<<V<<endl;
+	// cout<<"E: "<<E<<endl;
+
+	Graph *G;
+	G = new Graph;
+	G->numVertices = V;
+	G->adjLists = new node* [V];
+
+	for (int i = 0; i < V; ++i)
+	{
+		(G->adjLists)[i]=NULL;
+	}
+
+
+
+	for(int i=0;i<E;i++)
+	{
+		int node1,node2;
+		input>>node1>>node2;
+
+		node *temp;
+
+		if(G->adjLists[node1-1] == NULL)
+		{
+			G->adjLists[node1-1] = new node;
+			G->adjLists[node1-1]->vertex = node2;
+			G->adjLists[node1-1]->next = NULL;
+		}
+		else
+		{
+			temp = new node;
+			temp->vertex = node2;
+			temp->next = G->adjLists[node1-1]->next;
+
+			G->adjLists[node1-1]->next = temp;
+		}
+
+
+	}
+
+
+	// PRINT ADJACENCY LIST
+
+	/*for(int i=0;i<V;i++)
+	{
+		while(G->adjLists[i]!=NULL)
+		{
+			cout<<(G->adjLists[i])->vertex<<" ";
+			G->adjLists[i] = G->adjLists[i]->next;
+		}
+		cout<<endl;
+
+	}*/
+
+
+	Edge edge[E];
+	int in_degree[V+1];
+	//counting in degrees
+	for (int i = 1; i < (V+1); ++i)
+	{
+		in_degree[i]=0;
+	}
+
+	for (int i = 0; i < V; ++i)
+	{
+		node *temp;
+		temp = G->adjLists[i];
+
+		while(temp!=NULL)
+		{
+			in_degree[temp->vertex]++;
+			temp = temp->next;
+		}
+	}
+
+	//PRINT IN_DEGREES
+
+	/*for (int i = 1; i < (V+1); ++i)
+	{
+		cout<<"in_degree of"<<i<<": "<<in_degree[i]<<endl;
+	}*/
+
+	int count=0;
+
+	for (int i = 0; i < V; ++i)
+	{
+		node *temp;
+		temp = G->adjLists[i];
+
+		while(temp!=NULL)
+		{
+			edge[count].to = temp->vertex;
+			edge[count].from = i+1;
+			edge[count].weight = in_degree[i+1] + in_degree[temp->vertex];
+			count++;
+			temp = temp->next;
+		}
+	}
+
+	merge_sort(edge,0,E-1);
+
+	for (int i = 0; i < E; ++i)
+	{
+		// cout<<edge[i].from<<" "<<edge[i].weight<<" "<<edge[i].to<<endl;
+		output<<edge[i].from<<" "<<edge[i].weight<<" "<<edge[i].to<<"\n";
+	}
+
+
+	
+	return 0;
 }
